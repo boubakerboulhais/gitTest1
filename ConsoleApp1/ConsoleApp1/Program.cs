4771317a7ca93485ae3c7657de18568a010d068e
boubaker boulhais <boubakerboulhais@hotmail.com>
2017-12-24 12:43:35 +0100

    modification from visual studio

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             // code was added in github
             // code was edited in git hub
+            // code was added in visual studio
         }
     }
 }
